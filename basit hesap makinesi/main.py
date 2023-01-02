ea16e4e1b507cbd3de23435728fecce463a570f3
canbeytekin <canbytekin@gmail.com>
2023-01-02 04:44:27 +0300

    Create main.py

---
+++ basit hesap makinesi/main.py
@@ -0,0 +1,31 @@
+print("""***************************************
+HESAP MAKİNESİ
+
+1.Toplama İşlemi
+2.Çıkarma İşlemi
+3.Çarpma İşlemi
+4.Bölme İşlemi
+***************************************""")
+
+a = int(input("Birinci Sayıyı Giriniz:"))                                                #kullanıcıdan tam sayı girmesini istedik ve bunlar a,b olarak iki değere atadık.
+b = int(input("İkinci Sayıyı Giriniz:"))
+
+islem = input("Yapmak İstediğiniz İşlemi Seçin:")                                        #kullanıcıdan yapmak istediği işlemi yukarıda belirtilen işlemlerin karşılığı olan sayıya göre seçmesini istedik.
+
+if islem == "1":                                                                         #kullanıcının girdiği 4 işlem değerlerinin eşit olduğu koşullarda nasıl tepki vereceğine göre işlemleri sıraladık.
+    print("{} + {} = {}".format(a,b,a+b))                                            
+    print("İşleminiz Sonlandı..")
+elif islem == "2":
+    print("{} - {} = {}".format(a,b,a-b))
+    print("İşleminiz Sonlandı..")
+elif islem == "3":
+    print("{} x {} = {}".format(a,b,a*b))
+    print("İşleminiz Sonlandı..")
+elif islem == "4":
+    print("{} / {} = {}".format(a,b,a/b))
+    print("İşleminiz Sonlandı..")
+else:
+    print("Lütfen Geçerli Bir İşlem Seçiniz!!")
+    print("İşleminiz Sonlandı..")
+
+print("nacWARE")
